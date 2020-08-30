e28e888b7dc2904da69f0bde8b067a3249a67b84
PeradzeTsotne <tsotne.peradze.1@iliauni.edu.ge>
2020-08-31 00:09:01 +0400

    ....

--- Style/app.js
+++ Style/app.js
@@ -1,10 +1,29 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li');
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle('fade');
-    })
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+}
+btn.on('click', function (e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: 0
+    }, "300");
+    scrollDuration = 700;
 });
